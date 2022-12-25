5352e4c0d36582fc6274a388366aa652c9a5fbec
AdityaBattin <aditya.g.battin@gmail.com>
2022-12-25 14:38:00 +0530

    completed Footer

--- index.js
+++ index.js
@@ -4,6 +4,18 @@
 // for preloader
 // var loader = document.getElementById("loader");
 
+// logic for scroll spy
+
+let home = document.getElementById("myCarousel");
+let aboutUs = document.getElementById("About-us-section");
+let purvarang = document.getElementById("purvarang");
+let committees = document.getElementById("committees");
+
+let Navhome = document.getElementById("home");
+let NavaboutUs = document.getElementById("aboutus");
+let Navpurva = document.getElementById("purva");
+let Navcomm = document.getElementById("comm");
+
 //for navbar in small screens
 const navMenu = document.getElementById("navMenu");
 toggleMenu = document.getElementById("header_menu");
@@ -33,5 +45,39 @@ window.addEventListener("scroll", () => {
     header.classList.add("bgcolor-scrolled");
   } else {
     header.classList.remove("bgcolor-scrolled");
+  }
+});
+
+// logic for scroll scroll spy
+
+window.addEventListener("scroll", () => {
+  var windo = window.pageYOffset;
+  if (aboutUs.offsetTop <= windo + 100 && purvarang.offsetTop > windo) {
+    console.log("aboutUs");
+    Navhome.classList.remove("active");
+    NavaboutUs.classList.add("active");
+    Navpurva.classList.remove("active");
+    Navcomm.classList.remove("active");
+  } else if (
+    purvarang.offsetTop <= windo + 100 &&
+    committees.offsetTop > windo
+  ) {
+    console.log("purvarang");
+    Navhome.classList.remove("active");
+    NavaboutUs.classList.remove("active");
+    Navpurva.classList.add("active");
+    Navcomm.classList.remove("active");
+  } else if (committees.offsetTop <= windo + 100) {
+    console.log("commitees");
+    Navhome.classList.remove("active");
+    NavaboutUs.classList.remove("active");
+    Navpurva.classList.remove("active");
+    Navcomm.classList.add("active");
+  } else {
+    console.log("home");
+    Navhome.classList.add("active");
+    NavaboutUs.classList.remove("active");
+    Navpurva.classList.remove("active");
+    Navcomm.classList.remove("active");
   }
 });
